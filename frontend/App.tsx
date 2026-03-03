d31f7c1a4ee925a420dc28bedce94a27db4d05e9
Dhruv Bhardwaj <11dhruv.b@gmail.com>
2026-03-03 18:34:50 -0500

    Frontend Expo Router dashboard + theme styling and Navigation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from "./src/navigation/AppNavigator";
+import "expo-router/entry";
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
-
-export default App
