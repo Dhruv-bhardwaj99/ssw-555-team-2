b7b4a208fe1087f8cd673c30438c73fc71592d16
apothuri-hash <apothuri@stevens.edu>
2026-03-19 23:51:15 -0400

    Fix backend connectivity using Expo config and environment variables

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,2 +1,16 @@
+
+//export const API_BASE_URL =
+  //process.env.API_BASE_URL || "http://localhost:5000";
+// optional debug
+//console.log("ENV VALUE:", process.env.API_BASE_URL);
+
+
+//export const API_BASE_URL = "http://192.168.5.195:5000"; //created by Akhila for testing process so ignore. 
+
+
+import Constants from "expo-constants";
+
 export const API_BASE_URL =
-  process.env.EXPO_PUBLIC_API_BASE_URL || "http://localhost:5000";
+  Constants.expoConfig?.extra?.API_BASE_URL || "http://localhost:5000";
+
+console.log("API URL:", API_BASE_URL);
